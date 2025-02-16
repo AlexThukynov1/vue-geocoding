617e5d9a67c801a2f5fc5fee38742c181141761d
AlexThukynov1 <alexthukynov@gmail.com>
2025-02-16 21:20:59 +0200

    add client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require ("axios");
 require("dotenv").config();
 
 
@@ -9,16 +8,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
-app.get('/api/search:query',async (req, res) => {
-    try {
-        const query = req.params.query;
-        const result = await axios (`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`);
-
-        const data = result.data
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+app.use('/api/search:query', require("./routes/searchResults.js"))
 
 app.listen(PORT, () => console.log( `Started on port: ${PORT}`));
