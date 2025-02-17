18303c5008e662ba6d3f8bbea02a93354b5f8640
AlexThukynov1 <alexthukynov@gmail.com>
2025-02-17 23:15:53 +0200

    correction client

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
+import router from './router/index'
 import './style.css'
 import App from './App.vue'
-import {router} from "vue-router"
 
 const app = createApp(App)
 app.use(router)
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import HomeView from '../pages/HomeView.vue'
+
 const routes = [
   { path: '/', component: HomeView },
 ]
@@ -9,3 +10,5 @@ const router = createRouter({
   history: createMemoryHistory(),
   routes,
 })
+
+export default router
