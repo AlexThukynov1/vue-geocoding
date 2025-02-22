931191dd6b1da9dcdafbfb4dbe89e707fff94a80
AlexThukynov1 <alexthukynov@gmail.com>
2025-02-22 11:44:18 +0200

    add user location and custom marker

--- client/src/pages/HomeView.vue
+++ client/src/pages/HomeView.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import leaflet from 'leaflet'
+import leaflet, { PolyUtil } from 'leaflet'
 import { onMounted, ref } from 'vue';
 
 export default {
@@ -15,20 +15,55 @@ export default {
         onMounted(() => {
             map = leaflet.map = L.map('map').setView([50.45000, 30.52333], 10);
 
-            leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            accessToken: import.meta.env.CLIENT_APP_API_KEY,
-}).addTo(map);
+            leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
+            {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                accessToken: import.meta.env.CLIENT_APP_API_KEY,
+            })
+            .addTo(map);
+
+            getGeolocation();
         })
         
         const coords = ref(null);
         const fetchCoords = ref(null);
         const marker = ref(null);
+
         const getGeolocation = () => {
-            
+            fetchCoords.value = true;
+            navigator.geolocation.getCurrentPosition(setCoords, getLocError)
         }
-    
+
+        const setCoords = (pos) => {
+            fetchCoords.value = null;
+
+            const setSessionCoords = {
+                lat: pos.coords.latitude,
+                lng: pos.coors.longitude
+            };
+
+            sessionStorage.setItem("coords", JSON.stringify(setSessionCoords));
+
+            coords.value = setSessionCoords;
+
+            plotGeolocation(coords,value);
+        };
+
+        const getLocError = (e) => {
+            console.log(e)
+        };
+
+        const plotGeolocation = (coords) => {
+            const customMarket = leaflet.icon({
+                iconUrl: require('../assets/map-marker-red.svg'),
+                iconSize: [35, 35]
+            })
+
+            geoMarker.value = leaflet.marker([coords.lat, coords.lng], {icon: customMarket})
+            .addTo(map)
+        }
+        return {coords, geoMarker}
     }
 }
 </script>
